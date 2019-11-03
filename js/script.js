634b44a64c7020667356022e42d6d90060738e30
Paweł Kobylarski <cobzarocks@gmail.com>
2019-11-03 21:40:46 +0100

    nowe

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// const siteHeader = document.getElementById("site-header");
+
+// // window.addEventListener("scroll", e => {
+// //   if (window.scrollY > 0) {
+// //     siteHeader.classList.add("scroll");
+// //   } else {
+// //     siteHeader.classList.remove("scroll");
+// //   }
+// // });
+
+// window.addEventListener("scroll", function(e) {
+//   if (window.scrollY > 0) {
+//     siteHeader.classList.add("scroll");
+//   } else {
+//     siteHeader.classList.remove("scroll");
+//   }
+// });
+
+// const siteNav = document.getElementById("site-nav");
+// const menuToggle = document.getElementById("menu-toggle");
+
+// // menuToggle.addEventListener("click", () => {
+// //   siteNav.classList.toggle("expanded");
+// // });
+
+// menuToggle.addEventListener("click", function() {
+//   siteNav.classList.toggle("expanded");
+// });
+
+// // window.addEventListener("scroll", function(e) {
+
+// //   if (
+// //     section.getBoundingClientRect().bottom >= 0 &&
+// //     section.getBoundingClientRect().bottom <=
+// //       window.innerHeight + section.getBoundingClientRect().height
+// //   ) {
+// //     console.log(sect;
+// //   }
+// // });
